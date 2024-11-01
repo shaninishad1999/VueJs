f9e2d4b412081f91be518b103793d2f4ce2ffe71
shaninishad1999 <shaninishad1999@gmail.com>
2024-11-02 01:43:41 +0530

    Vuejs

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Welcome to My Vue App</h1>
+    </header>
+    <main>
+      <HelloWorld msg="Hello from App.vue!" />
+    </main>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+
+header {
+  background-color: #42b983;
+  padding: 20px;
+  color: white;
+}
+</style>
---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.hello {
+  font-size: 1.5em;
+  color: #35495e;
+}
+</style>
